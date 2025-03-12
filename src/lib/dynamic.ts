bf08c107e1a55f7b477b56307b6a34a4fc6d2532
Félix Dubé <felix.dube@moulin.social>
2025-03-12 12:55:49 -0400

    simplify

--- src/lib/dynamic.ts
+++ src/lib/dynamic.ts
@@ -4,8 +4,5 @@ import dynamic from "next/dynamic";
 
 // Export the dynamic component that will trigger the Turbopack issue
 export const DynamicComponent = dynamic(
-  () => import("../components/DynamicComponent"),
-  {
-    ssr: false
-  }
+  () => import("../components/DynamicComponent")
 );
