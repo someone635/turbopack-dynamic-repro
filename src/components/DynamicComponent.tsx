fb14494590592708779b34e64ffa0c843dc054bb
Félix Dubé <felix.dube@moulin.social>
2025-03-12 13:29:27 -0400

    remove ai generated noise

--- src/components/DynamicComponent.tsx
+++ src/components/DynamicComponent.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 
 export default function DynamicComponent() {
-    return (
-        <div className="p-4 bg-blue-100 rounded">
-            <h2 className="text-lg font-bold">Dynamic Component</h2>
-            <p>This component is dynamically imported</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>This component is dynamically imported</p>
+    </div>
+  );
 }
